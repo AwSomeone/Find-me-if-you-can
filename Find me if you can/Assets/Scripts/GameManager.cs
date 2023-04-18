6b3feaf923f15344cc081ebd9cd851185ff6e2f7
Jiro Sowell <jirosow@icloud.com>
2023-04-18 23:00:59 +0200

    Added GameManager
    Now you can input what choices and videos should be shown and the GameManager will play the chosen videos and once having reached the end, the buttons will pop up, which once clicked, start the next video.

---
+++ Find me if you can/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using System;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    [Serializable]
+    public class StoryMap
+    {
+        [Space(10)]
+        public VideoClip video;
+        public float timeToThink;
+        public Choice[] choice;
+
+        //Add subtitle variable?
+    }
+
+    [Serializable]
+    public class Choice
+    {
+        public int leadsTo; //Which sequence the button should start
+        public Button button;
+
+        public GameManager gameManagerScript;
+        private Choice ()
+        {
+            button.onClick.AddListener(ClickFunction);
+        }
+        private void ClickFunction()
+        {
+            gameManagerScript.ToggleButtons(false);
+            gameManagerScript.StartSequence(leadsTo);
+        }
+    }
+
+    public StoryMap[] sequences;
+    //Above is the code for what can be seen on the script component in the inspector.
+    //There you can put the video clips to be shown and link up buttons to make them start the next videos.
+
+    private VideoPlayer player;
+    private int currentSequence;
+
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        StartSequence(0);
+    }
+
+
+    //Starts Video
+    private void StartSequence(int sequence)
+    {
+        player.clip = sequences[sequence].video;
+        player.Play();
+        player.loopPointReached += EndReached; //Pauses video at end point
+    }
+
+    private void EndReached(VideoPlayer vp)
+    {
+        vp.playbackSpeed *= 0f;
+    }
+
+
+    //Sets up choices when video ends
+    private void NewChoice()
+    {
+        ToggleButtons(true);
+    }
+
+    private void ToggleButtons(bool makeVisible)
+    {
+        foreach(Choice choice in sequences[currentSequence].choice)
+        {
+            choice.button.gameObject.SetActive(true); //Brings up the choices on screen
+        }
+    }
+}
