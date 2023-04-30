de2cc0e3bcf9225f65c1fa61128c8804682b9eab
Ceci9501 <cecilia.950110@gmail.com>
2023-04-30 15:09:24 +0200

    Meny och options

--- Find me if you can/Assets/Scripts/MusicPlayer.cs
+++ Find me if you can/Assets/Scripts/MusicPlayer.cs
@@ -7,9 +7,12 @@ public class MusicPlayer : MonoBehaviour
 
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip musicClip;
+    public float musicVolume = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
+     
         audioSource.clip = musicClip;
         audioSource.loop = true;
         audioSource.Play();
@@ -18,6 +21,12 @@ public class MusicPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        audioSource.volume = musicVolume;
+    }
+
+   public void updateVolume(float volume)
+    {
+       
+        musicVolume = volume;
     }
 }
