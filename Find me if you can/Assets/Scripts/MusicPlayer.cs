4cd653c9a9f999d1c86869bea48f54560fa664a0
Ceci9501 <cecilia.950110@gmail.com>
2023-04-30 16:26:06 +0200

    Volume Slider
    Volume slider i options button funkar nu men bara där det finns musik inte ljudet från videos

--- Find me if you can/Assets/Scripts/MusicPlayer.cs
+++ Find me if you can/Assets/Scripts/MusicPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class MusicPlayer : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip musicClip;
     public float musicVolume = 1f;
+    public Slider volumeSlider;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,16 @@ public class MusicPlayer : MonoBehaviour
         audioSource.clip = musicClip;
         audioSource.loop = true;
         audioSource.Play();
+        musicVolume = PlayerPrefs.GetFloat("volume");
+        audioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
         audioSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
     }
 
    public void updateVolume(float volume)
