ad27e1cc358a421404890e9934b7396fdc63e702
malinfredriksson <malinfredriksson@live.com>
2023-05-14 19:23:40 +0200

    ändringar i work
    gjort om infobutton till notepad, fungerar inte riktigt som den ska

---
+++ Find me if you can/Assets/Scripts/Notepad.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Notepad : MonoBehaviour
+{
+    public string textInput;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void SaveText()
+    {
+        textInput = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = textInput;
+    }
+}
