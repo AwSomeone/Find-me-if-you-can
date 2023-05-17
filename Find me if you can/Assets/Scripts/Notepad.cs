a0e499c32063fd22cde70821a3bcb2cf44d3b882
malinfredriksson <malinfredriksson@live.com>
2023-05-17 23:36:54 +0200

    Uppdaterat kod i notepad
    försökt fixa så att texten sparas vid byte av scen men har inte löst det än

--- Find me if you can/Assets/Scripts/Notepad.cs
+++ Find me if you can/Assets/Scripts/Notepad.cs
@@ -8,15 +8,36 @@ public class Notepad : MonoBehaviour
 {
     public string textInput;
     public GameObject inputField;
-    public GameObject textDisplay;
+    //public GameObject textDisplay;
+    private string noteKey = "savedText";
 
+
+    private void Start()
+    {
+        //återställ vid start
+        PlayerPrefs.SetString(noteKey, "");
+    }
 
     public void SaveText()
     {
         textInput = inputField.GetComponent<Text>().text;
-        textDisplay.GetComponent<Text>().text = textInput;
+        //textDisplay.GetComponent<Text>().text = textInput;
+
+        Debug.Log("Input Field Text: " + textInput); // Kontrollera att texten hämtas korrekt
+        Debug.Log("Input Field Object: " + inputField.name);
+
+        PlayerPrefs.SetString(noteKey, textInput);
+        PlayerPrefs.Save();
+    }
+
+    private void OnEnable()
+    {
+        //if (PlayerPrefs.HasKey(noteKey))
+        //{
+            textInput = PlayerPrefs.GetString(noteKey);
+            //textDisplay.GetComponent<Text>().text = textInput;
+        //}
     }
 
 
-   
 }
